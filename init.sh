11faa8b0bb9b8e7bae7e8270811dda778b2c1152
wshudong <wsd312@163.com>
2016-10-12 13:08:06 +0800

    init

---
+++ init.sh
@@ -0,0 +1,6 @@
+git clone https://github.com/someet/blog.git
+npm install 
+
+git clone https://github.com/tufu9441/maupassant-hexo.git themes/maupassant
+npm install hexo-renderer-jade --save
+npm install hexo-renderer-sass --save
